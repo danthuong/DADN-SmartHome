96d6ea44a78d1f74e8abfa0b4c8a4c1fbc08e4a6
danthuong <sugarpeach2k5@gmail.com>
2026-05-11 22:28:14 +0700

    update

--- mobile_app/app/src/main/java/com/example/android_app/data/api/ApiClient.kt
+++ mobile_app/app/src/main/java/com/example/android_app/data/api/ApiClient.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit
 
 object ApiClient {
     // THAY ĐỔI IP NÀY THEO MÁY CỦA BẠN
-    private const val BASE_URL = "http://192.168.88.92:8000/"
+    private const val BASE_URL = "http://100.126.85.58:8000/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
