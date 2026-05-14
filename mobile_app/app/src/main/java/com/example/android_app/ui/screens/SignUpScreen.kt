18151e96d0c2c00fc76fa9873dae907161f0ca68
danthuong <sugarpeach2k5@gmail.com>
2026-05-14 17:51:14 +0700

    add dependency

--- mobile_app/app/src/main/java/com/example/android_app/ui/screens/SignUpScreen.kt
+++ mobile_app/app/src/main/java/com/example/android_app/ui/screens/SignUpScreen.kt
@@ -1,5 +1,6 @@
 package com.example.android_app.ui.screens
 
+import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
